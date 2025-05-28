5ad4476a7b81f924288ff4a5a39e8f7f3bd950d5
adm_phivert <paul.hivert@tessi.fr>
2025-05-28 18:12:38 +0200

    update mason

--- lua/phivert/lazy.lua
+++ lua/phivert/lazy.lua
@@ -54,22 +54,18 @@ require('lazy').setup({
 	{
 		"NvChad/nvterm",
 	},
-	-- lsp zero (manson)
+	-- manson (lsp)
 	{
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v3.x',
-		dependencies =
-		{
-		      --- manage LSP servers
-		      'williamboman/mason.nvim',
-		      'williamboman/mason-lspconfig.nvim',
-		      -- LSP Support
-		      'neovim/nvim-lspconfig',
-		      -- Autocompletion
-		      'hrsh7th/nvim-cmp',
-		      'hrsh7th/cmp-nvim-lsp',
-		      'L3MON4D3/LuaSnip',
-		}
+        "mason-org/mason.nvim",
+        opts = {
+            ui = {
+            icons = {
+                package_installed = "✓",
+                package_pending = "➜",
+                package_uninstalled = "✗"
+                }
+            }
+        } 
 	},
 	-- dap + ui (dapui)	
 	{
